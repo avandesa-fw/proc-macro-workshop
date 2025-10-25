1207999cbbf368f1757fae96c3fc2bef9c970896
Alex van de Sandt <asandt@fieldwire.com>
2025-10-24 19:04:29 -0500

    derive_builder: test 03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -20,14 +20,20 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let builder = builder(&vis, &builder_name, &struct_fields);
     let builder_initializer = builder_initializer(&name, &builder_name, &struct_fields);
+    let setters = setters(&builder_name, &struct_fields);
 
-    output(builder, builder_initializer).into()
+    output(builder, builder_initializer, setters).into()
 }
 
-fn output(builder: TokenStream, builder_initializer: TokenStream) -> TokenStream {
+fn output(
+    builder: TokenStream,
+    builder_initializer: TokenStream,
+    setters: TokenStream,
+) -> TokenStream {
     quote! {
         #builder
         #builder_initializer
+        #setters
     }
 }
 
@@ -62,8 +68,28 @@ fn builder_initializer(
     }
 }
 
+fn setters(builder_name: &Ident, struct_fields: &FieldsNamed) -> TokenStream {
+    let setters = struct_fields.named.iter().map(setter);
+    quote! {
+        impl #builder_name {
+            #(#setters)*
+        }
+    }
+}
+
+fn setter(field: &Field) -> TokenStream {
+    let name = field.ident.as_ref().expect("named field");
+    let ty = &field.ty;
+    quote! {
+        pub fn #name(&mut self, #name: #ty) -> &mut Self {
+            self.#name = ::std::option::Option::Some(#name);
+            self
+        }
+    }
+}
+
 fn optionalize_field(field: &Field) -> TokenStream {
-    let name = &field.ident;
+    let name = field.ident.as_ref().expect("named field");
     let field_ty = &field.ty;
     quote! { #name: ::std::option::Option<#field_ty> }
 }
