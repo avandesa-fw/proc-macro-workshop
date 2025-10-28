d6897504d07b1b3a6ff47045bbd941cbdf492b85
Alex van de Sandt <asandt@fieldwire.com>
2025-10-28 15:07:07 -0500

    sorted: test 01

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,13 @@
 use proc_macro::TokenStream;
+use quote::ToTokens;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
 
-    unimplemented!()
+    let item = parse_macro_input!(input as syn::Item);
+    dbg!(&item);
+
+    item.into_token_stream().into()
 }
